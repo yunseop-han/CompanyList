41bf01a10d57affefb963388e51753520bdfd57a
yunseop.han <yunseop.han@gmail.com>
2020-09-09 13:50:34 +0900

    Initial Commit

---
+++ CompanyList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CompanyList
+//
+//  Created by 한윤섭 on 2020/09/09.
+//  Copyright © 2020 yunseop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
